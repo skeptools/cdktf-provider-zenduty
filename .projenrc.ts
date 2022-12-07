05199061e177e63c1e2f4972f4269b49d1ee1dfe
Ryan Martin <ryan@vestahealthcare.com>
2022-12-07 14:23:04 -0500

    A couple fixes to projenrc.ts

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,19 @@
 import { CdktfProviderProject } from "@cdktf/provider-project";
+import { NpmAccess } from "projen/lib/javascript";
+
 const project = new CdktfProviderProject({
   author: "Ryan Martin",
-  authorAddress: "ryan@vestahealthcare.com",
+  authorAddress: "rlmartin@gmail.com",
+  cdktfVersion: "^0.14.0",
+  constructsVersion: "^10.0.0",
   defaultReleaseBranch: "main",
   devDeps: ["@cdktf/provider-project"],
-  name: "cdktf-provider-zenduty",
+  name: "@skeptools/cdktf-provider-zenduty",
   projenrcTs: true,
   repositoryUrl: "git@github.com:skeptools/cdktf-provider-zenduty.git",
   terraformProvider: "Zenduty/zenduty@~> 0",
+  releaseToNpm: true,
+  npmAccess: NpmAccess.PUBLIC,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */

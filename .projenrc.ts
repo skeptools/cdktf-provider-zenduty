fe87a9a774e3581aa1f4d996bf871f7e313642a5
Ryan Martin <rlmartin@gmail.com>
2024-11-15 07:46:54 -0500

    fix: use Node 18

--- .projenrc.ts
+++ .projenrc.ts
@@ -20,6 +20,7 @@ const project = new CdktfProviderProject({
   terraformProvider: "Zenduty/zenduty@~> 0",
   releaseToNpm: true,
   npmAccess: NpmAccess.PUBLIC,
+  minNodeVersion: '18.12.0',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
